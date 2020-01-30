0b1c62369dca18d85bff2033a2106ddb1cc10cb2
TJ the Developer <tjdoescode@gmail.com>
2020-01-30 09:54:04 -0600

    main route working, vote non-functional

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const fs = require("fs");
 
+app.use(express.static(__dirname));
+
+app.post('/', function (req, res) {
+    console.log('index');
+    res.send("test");
+});
+
 app.get('/vote', (req, res) => {
+    console.log(req.body)
     vote(req.query[0], req.query[1])
     res.send('success');
 });
 
-function vote(id, opt) {
-    if (id == null || opt == null) retrn;
+function vote(iD, opt) {
+    //console.log(iD);
+    if (iD == null || opt == null) return;
     let raw = fs.readFileSync('votes.json');
     let votes = JSON.parse(raw);
+    let id = parseInt(iD);
 
-    votes.id[opt]++;
-    console.log(votes);
+    console.log(votes.votes[id]);
+    console.log(raw);
+    //console.log(JSON.stringify(votes, null, 2));
+    //fs.writeFileSync("./votes.json", votes);
 }
 
-app.listen(3000, () => console.log('Listening on 3000'));
+app.listen(8080, () => console.log('Listening'));
