66fdfc70d80c446a10e42bfc80e3a02e75c62a4c
TJ the Developer <tjdoescode@gmail.com>
2020-02-03 10:32:02 -0600

    First functioning skeleton POST /vote funtional JSON structure fixed ISSUE: JSON not able to be dynamically updated

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser = require('body-parser');
 const fs = require("fs");
 
 app.use(express.static(__dirname));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
     console.log('index');
     res.send("test");
 });
 
-app.get('/vote', (req, res) => {
-    console.log(req.body)
-    vote(req.query[0], req.query[1])
+app.post('/vote', (req, res) => {
+    console.log(req.body);
+    vote(req.body.id, req.body.value);
     res.send('success');
 });
 
 function vote(iD, opt) {
-    //console.log(iD);
+    console.log(iD);
     if (iD == null || opt == null) return;
     let raw = fs.readFileSync('votes.json');
-    let votes = JSON.parse(raw);
+    let data = JSON.parse(raw);
     let id = parseInt(iD);
 
-    console.log(votes.votes[id]);
-    console.log(raw);
-    //console.log(JSON.stringify(votes, null, 2));
-    //fs.writeFileSync("./votes.json", votes);
+    console.log(data.votes.n1);
+
+    let nNum = `n${id}`;
+    let cNum = `c${opt}`;
+
+    data.votes.nNum.cNum++;
+
+    let upData = JSON.stringify(data, null, 2);
+    fs.writeFileSync("./votes.json", upData);
 }
 
 app.listen(8080, () => console.log('Listening'));
